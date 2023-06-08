7ce917be9bbb7c3251b253cd4a107eb3c5a74c60
Magnilson Teixeira <magnilson.teixeira@viavarejo.com.br>
2023-06-08 10:34:38 -0300

    chore(spring): configuracao inicial do projeto spring 2.7.13 java 17

---
+++ src/main/java/com/magnilsonti/treinamentoudemy/SpringJava17Junit5Application.java
@@ -0,0 +1,13 @@
+package com.magnilsonti.treinamentoudemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJava17Junit5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJava17Junit5Application.class, args);
+}
+
+}
---
+++ src/test/java/com/magnilsonti/treinamentoudemyu/SpringJava17Junit5ApplicationTests.java
@@ -0,0 +1,14 @@
+package com.magnilsonti.treinamentoudemyu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJava17Junit5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+		
+	}
+
+}
